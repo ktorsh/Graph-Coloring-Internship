a3902b782c2c9b1dbdd98aaa0dcfa3f21644a7a7
ktorsh <gtorshizi@gmail.com>
2019-07-16 12:40:24 -0400

    Add in no update algos

---
+++ src/algorithms/SixColorLowDegreeNoUpdate.java
@@ -0,0 +1,49 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import graphs.Colors;
+import graphs.GraphMatrix;
+
+public class SixColorLowDegreeNoUpdate implements ColoringAlgo{
+	GraphMatrix graph;
+	int[][] adjMatrix; 
+	public SixColorLowDegreeNoUpdate(GraphMatrix g) { 
+		graph = g; 
+		adjMatrix = g.getAdjacencyMatrix();
+	}
+	public void color() { 
+		Stack<Integer> stack = new Stack<Integer>();
+		ArrayList<Integer> connectedNodes = new ArrayList<Integer>(adjMatrix.length); //which nodes are in play
+		for (int i = 0; i < adjMatrix.length; i++) { 
+			connectedNodes.add(i);
+		}
+		for (int j = 0; j < adjMatrix.length; j++) { //find node with least edges
+			int minNode = connectedNodes.get(0); 
+			for (int i = 1; i < connectedNodes.size(); i++) {
+				if (graph.getVertexDegree(connectedNodes.get(i)) < graph.getVertexDegree(minNode)) { 
+					minNode = connectedNodes.get(i);
+				}
+			}
+			connectedNodes.remove(Integer.valueOf(minNode));
+			stack.push(minNode);
+		}
+		while (!stack.empty()) { 
+			int node = stack.pop();
+			boolean hasColor = false;
+			int currentColor = -1; 
+			while (!hasColor) {
+				currentColor++;
+				hasColor = true;
+				for (int i = 0; i < adjMatrix.length; i++) { //colors with the "least" available color
+					if (adjMatrix[node][i] == 1 && currentColor == graph.getNodeColor(i).getN()) { 
+						hasColor = false;				
+					}
+				}
+			}
+			graph.setNodeColor(node, Colors.values()[currentColor+1]);
+		}
+	}
+
+}
---
+++ src/algorithms/SixColorRecursiveNoUpdate.java
@@ -0,0 +1,59 @@
+package algorithms;
+
+import graphs.Colors;
+import graphs.GraphMatrix;
+
+public class SixColorRecursiveNoUpdate implements ColoringAlgo{
+	GraphMatrix graph;
+	int[][] adjMatrix;
+	public SixColorRecursiveNoUpdate(GraphMatrix g) { 
+		graph = g; 
+		adjMatrix = g.getAdjacencyMatrix();
+	}
+	public void color() { 
+		int[] conNodes = new int[adjMatrix.length]; 
+		for (int i = 0; i < adjMatrix.length; i++) { 
+			conNodes[i] = i;
+		}
+		color(conNodes);
+	}
+	private void color(int[] connectedNodes) { 
+		if (connectedNodes.length == 1) { 
+			graph.setNodeColor(connectedNodes[0], Colors.RED);
+			return; 
+		}
+		int minNode = connectedNodes[0]; 
+		for (int i = 1; i < connectedNodes.length; i++) {
+			if (graph.getVertexDegree(connectedNodes[i]) < graph.getVertexDegree(minNode)) { 
+				minNode = connectedNodes[i];
+			}
+		}
+		int[] newCon = new int[connectedNodes.length - 1];
+		int counter = 0;
+		for (int j = 0; j < connectedNodes.length; j++) {
+			System.out.print(connectedNodes[j]+" ");
+		}
+		System.out.println();
+		System.out.println(minNode);
+		for (int i = 0; i < connectedNodes.length; i++) { 
+			if (connectedNodes[i] != minNode) { 
+				newCon[counter] = connectedNodes[i]; 
+				counter++;
+			}
+		}
+		color(newCon); 
+		boolean hasColor = false;
+		int currentColor = -1; 
+		while (!hasColor) {
+			currentColor++;
+			hasColor = true;
+			for (int i = 0; i < adjMatrix.length; i++) { 
+				if (adjMatrix[minNode][i] == 1 && currentColor == graph.getNodeColor(i).getN()) { 
+					hasColor = false;				
+				}
+			}
+		}
+		graph.setNodeColor(minNode, Colors.values()[currentColor+1]);
+	}
+
+}
